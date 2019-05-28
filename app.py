e50289e6194891ffe86a69736b5a737556f12e68
Bendricks40 <bendricks40@gmail.com>
2019-05-28 14:53:54 -0400

    adding teams parsing

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import constants
 def run_app():
 
     myPlayers = []
+    myTeams = []
 
     for player in constants.PLAYERS:
         myPlayers.append(player)
+    for team in constants.TEAMS:
+        myTeams.append(team)
     print(myPlayers)
-
+    print(myTeams)
 
 
 if __name__ == '__main__':
