7a45ab4b004cac6b975dd591307b9e505ae68982
Bendricks40 <bendricks40@gmail.com>
2019-05-28 15:04:38 -0400

    initial console window

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ def run_app():
         myPlayers.append(player)
     for team in constants.TEAMS:
         myTeams.append(team)
-    print(myPlayers)
-    print(myTeams)
 
+    print("\n\nWelcome to the Team Stats Tool! \n\n")
+
+    option = input("What would you like to do first?\n\n\n"
+          "Enter one of the following options: \n\n"
+          "1. Display Team Stats \n"
+          "2. Quit \n\n"
+          "Enter choice here: ")
+
+    if option.upper() =="1":
+        pass
+        print("Goodbye!!!!!!!!!!!!!!!")
+    elif option.upper() =="2":
+        print("hello theareslksadfj")
 
 if __name__ == '__main__':
     run_app()
