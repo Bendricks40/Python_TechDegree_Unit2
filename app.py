8b14b100993de81716c86f65e6568f5966df95a4
Bendricks40 <bendricks40@gmail.com>
2019-05-30 09:09:09 -0400

    looping through players and adding a team to each

--- app.py
+++ app.py
@@ -11,6 +11,20 @@ def run_app():
     for team in constants.TEAMS:
         myTeams.append(team)
 
+    print(len(myPlayers))
+
+    teamNum = 0
+
+    for player in myPlayers:
+        if teamNum == 3:
+            teamNum = 0
+        player.update({"Team": myTeams[teamNum]})
+        teamNum += 1
+
+
+    for player in myPlayers:
+        print(player)
+
     print("\n\nWelcome to the Team Stats Tool! \n\n")
 
     option = input("What would you like to do first?\n\n\n"
@@ -19,11 +33,13 @@ def run_app():
           "2. Quit \n\n"
           "Enter choice here: ")
 
-    if option.upper() =="1":
-        pass
+    if option.upper() == "1":
+        for team in myTeams:
+            print(team)
         print("Goodbye!!!!!!!!!!!!!!!")
-    elif option.upper() =="2":
+    elif option.upper() == "2":
         print("hello theareslksadfj")
+
 
 if __name__ == '__main__':
     run_app()
